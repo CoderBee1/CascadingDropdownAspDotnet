19d37a68c90cf59ab91c91612946d2333f14607e
CoderBee1 <CoderBee@DESKTOP-ALSSB75>
2023-02-27 03:17:13 +0400

    first commit of this project

---
+++ WebApi/Controllers/DropdownListController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class DropdownListController : ApiController
+    {
+        Altkamul_LibraryEntities db = new Altkamul_LibraryEntities();
+
+        [HttpGet]
+        public IHttpActionResult Index(int id)
+        {
+            List<Author> obj = new List<Author>();
+            obj = db.Authors.Where(x => x.AuthorId== id).ToList();
+            return Ok(obj[0].AuthorName);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetBook()
+        {
+            var bookTitle = db.Books.ToList();
+            //var obj = db.Books.Where(model => model.BookId == bid).FirstOrDefault();
+            return Ok(bookTitle);
+        }
+    }
+}
